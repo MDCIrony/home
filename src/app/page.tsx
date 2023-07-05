8c7e655d73e51270dbc4d2252799160a0b16d029
MDCIrony <miguel.acastillodiaz@outlook.es>
2023-07-05 18:48:52 -0500

    Testing lintern and prettier configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,37 +11,62 @@ const Cards: ICard[] = [
     {
         title: "Docs",
         body: "Find in-depth information about Next.js features and API.",
-        href: "https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app",
+        href: "https://nextjs.org/docs?utm_source=create-next-app&utm_medium=\
+        appdir-template&utm_campaign=create-next-app",
     },
     {
         title: "Learn",
         body: "Learn about Next.js in an interactive course with&nbsp;quizzes!",
-        href: "https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app",
+        href: "https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir\
+        -template-tw&utm_campaign=create-next-app",
     },
     {
         title: "Templates",
         body: "Explore the Next.js 13 playground.",
-        href: "https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app",
+        href: "https://vercel.com/templates?framework=next.js&utm_source=create-next\
+        -app&utm_medium=appdir-template&utm_campaign=create-next-app",
     },
     {
         title: "Deploy",
         body: "Instantly deploy your Next.js site to a shareable URL with Vercel.",
-        href: "https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app",
+        href: "https://vercel.com/new?utm_source=create-next-app&utm_medium\
+        =appdir-template&utm_campaign=create-next-app",
     },
 ];
+
+const classNames = {
+    getStarted:
+        "fixed left-0 top-0 flex w-full justify-center border-b border-gray-300\
+    bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800\
+    dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border\
+    lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30",
+
+    nextLogo:
+        "relative flex place-items-center before:absolute before:h-[300px] before:w-[480px]\
+    before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white\
+    before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px]\
+    after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200\
+    after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent\
+    before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff]\
+    after:dark:opacity-40 before:lg:h-[360px]",
+};
 
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-                <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+                <p className={classNames.getStarted}>
                     Get started by a&nbsp;
                     <code className="font-mono font-bold">src/app/page.tsx</code>
                 </p>
-                <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+                <div
+                    className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t
+                    from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none"
+                >
                     <a
                         className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-                        href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                        href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign\
+                        =create-next-app"
                         target="_blank"
                         rel="noopener noreferrer"
                     >
@@ -58,10 +83,7 @@ export default function Home() {
                 </div>
             </div>
 
-            <div
-                className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px]
-       before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]"
-            >
+            <div className={classNames.nextLogo}>
                 <Image
                     className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
                     src="/next.svg"
@@ -78,13 +100,18 @@ export default function Home() {
                         <a
                             key={Card.title}
                             href={Card.href}
-                            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+                            className="group rounded-lg border border-transparent px-5 py-4 transition-colors
+                            hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700
+                            hover:dark:bg-neutral-800/30"
                             target="_blank"
                             rel="noopener noreferrer"
                         >
                             <h2 className={`mb-3 text-2xl font-semibold`}>
                                 {`${Card.title} `}
-                                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                                <span
+                                    className="inline-block transition-transform group-hover:translate-x-1
+                                    motion-reduce:transform-none"
+                                >
                                     -&gt;
                                 </span>
                             </h2>
